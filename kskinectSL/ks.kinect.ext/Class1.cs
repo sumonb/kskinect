bf783cf38b9f20220cb1c807e75655e7505f6769
spyctg@yahoo.com <ksumonctg1>
2016-05-08 20:27:17 +1000

    first commit

---
+++ kskinectSL/ks.kinect.ext/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ks.kinect.ext
+{
+    public class Class1
+    {
+    }
+}
