94d574106ca4b8863b006ec64962e4fc258517a4
sumonb <spyctg@yahoo.com>
2016-05-10 10:00:43 +1000

    include library

---
+++ kskinectSL/ks.kinect.wpf/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ks.kinect.wpf
+{
+    public class Constants
+    {
+        public enum CameraMode
+        {
+            Color,
+            Depth,
+            Infrared
+        }
+    }
+}
--- kskinectSL/ks.kinect.wpf/MainWindow.xaml.cs
+++ kskinectSL/ks.kinect.wpf/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Microsoft.Kinect;
+using System.Windows.Media.Imaging;
+using ks.kinect.ext;
+using LightBuzz.Vitruvius;
+
 namespace ks.kinect.wpf
 {
     /// <summary>
@@ -20,9 +25,87 @@ namespace ks.kinect.wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly KinectSensor _sensor;
+        IList<Body> _bodies;
+
+        // Reads Color frame data
+        private readonly ColorFrameReader _colorReader = null;
+        private readonly InfraredFrameReader _infraredReader = null;
+        private readonly DepthFrameReader _depthReader = null;
+
+
+        private Constants.CameraMode _mode = Constants.CameraMode.Color;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _sensor = KinectSensor.GetDefault();
+            if (_sensor == null) return;
+
+
+            _colorReader.ColorFrameSource.OpenReader();
+            _colorReader.FrameArrived += ColorReader_FrameArrived;
+
+            _infraredReader.InfraredFrameSource.OpenReader();
+            _infraredReader.FrameArrived += InfraredReader_FrameArrived;
+
+            _depthReader.DepthFrameSource.OpenReader();
+            _depthReader.FrameArrived += DepthReader_FrameArrived;
+        }
+
+        void DepthReader_FrameArrived(object sender, DepthFrameArrivedEventArgs e)
+        {
+            if (_mode == Constants.CameraMode.Depth)
+            {
+                using (var frame = e.FrameReference.AcquireFrame())
+                {
+                    if (frame == null) return;
+                    camera.Source = frame.ToBitmap();
+                }
+            }
+        }
+
+
+
+        void ColorReader_FrameArrived(object sender, ColorFrameArrivedEventArgs e)
+        {
+            if (_mode == Constants.CameraMode.Color)
+            {
+                using (var frame = e.FrameReference.AcquireFrame())
+                {
+                    if (frame == null) return;
+                    camera.Source = frame.ToBitmap();
+                }
+            }
+
+        }
+
+        void InfraredReader_FrameArrived(object sender, InfraredFrameArrivedEventArgs e)
+        {
+            if (_mode == Constants.CameraMode.Infrared)
+            {
+                using (var frame = e.FrameReference.AcquireFrame())
+                {
+                    if (frame == null) return;
+                    camera.Source = frame.ToBitmap();
+                }
+            }
+        }
+
+
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (_colorReader != null)
+            {
+                _colorReader.Dispose();
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+            }
         }
     }
 }
